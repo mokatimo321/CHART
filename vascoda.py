9bdbbcaad99c079ae4d6026f848656009ec7cb24
mokatimo321 <99364832+mokatimo321@users.noreply.github.com>
2022-06-22 21:32:24 +0530

    Update vascoda.py

--- vascoda.py
+++ vascoda.py
@@ -7,10 +7,9 @@ Created on Wed Jun 22 21:05:08 2022
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 import os
-import webbrowser
-import datetime
+#import webbrowser
 import pandas as pd
 from datetime import datetime
 import base64
@@ -33,14 +32,14 @@ def filedownload(df):
 uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
 if uploaded_file is not None:
     input_df = pd.read_csv(uploaded_file)      
-    fig = go.Figure(data=[go.Candlestick(x=input_df['date'],
+    """fig = go.Figure(data=[go.Candlestick(x=input_df['date'],
                 open=input_df['open'],
                 high=input_df['high'],
                 low=input_df['low'],
                 close=input_df['close'])])
 
     fig.write_html("BN.html")
-    webbrowser.open_new_tab("BN.html")
+    webbrowser.open_new_tab("BN.html")"""
     st.markdown(filedownload(input_df), unsafe_allow_html=True)
 
 st.subheader('CHART')
